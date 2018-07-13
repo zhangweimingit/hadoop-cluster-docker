8c0b085d0e4f7d705eb064c2d443e056060f0c0e
wmzhang <wmzhang@ikuai8.com>
2018-07-13 14:42:40 +0800

    test

--- config/run-wordcount.sh
+++ config/run-wordcount.sh
@@ -6,7 +6,7 @@
 hadoop fs -mkdir -p input
 
 # put input files to HDFS
-hdfs dfs -put ./data/* input
+hdfs dfs -put ./input/* input
 
 # run wordcount 
 hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.1.0.jar \
@@ -15,14 +15,15 @@ hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.1.0.jar \
     -mapper ~/mapperC \
     -reducer ~/reduceC
 
+hdfs dfs -get output/* ./output
 # print the input files
-echo -e "\ninput file1.txt:"
-hdfs dfs -cat input/file1.txt
+#echo -e "\ninput file1.txt:"
+#hdfs dfs -cat input/file1.txt
 
-echo -e "\ninput file2.txt:"
-hdfs dfs -cat input/file2.txt
+#echo -e "\ninput file2.txt:"
+#hdfs dfs -cat input/file2.txt
 
 # print the output of wordcount
-echo -e "\nwordcount output:"
-hdfs dfs -cat output/*
+#echo -e "\nwordcount output:"
+#hdfs dfs -cat output/*
 
